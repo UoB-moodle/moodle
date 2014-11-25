145076e74aa15612869ba4b7404bd378f39a9abe
root <root@precise32.(none)>
2014-11-25 11:39:58 +0000

    Renamed to panoptobutton

---
+++ lib/editor/tinymce/plugins/panoptobutton/grabber.php
@@ -0,0 +1,23 @@
+<?php 
+/*Script to retrieve panopto course ID of moodle course in which the editor is open.
+ If no course ID is found, outputs nothing.*/
+require(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))) . '/config.php');
+
+
+//Function to retrieve Panopto course id given moodle course id.
+function get_panopto_course_id($moodle_course_id) {
+    global $DB;
+    return $DB->get_field('block_panopto_foldermap', 'panopto_id', array('moodleid' => $moodle_course_id));
+}
+
+$id = $COURSE->id;
+$panid = get_panopto_course_id($id);
+
+	if($panid){
+		echo('?externalId=' . $panid);
+	}
+	else{
+		echo("");
+	}
+
+?>
